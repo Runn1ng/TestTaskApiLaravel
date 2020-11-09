87751bf0e0183bcbf337a0e5ffe0754503035042
Runn1ng <39840625+Runn1ng@users.noreply.github.com>
2020-11-10 01:49:08 +0500

    Almost complete GET
    Почти сделал GET запрос, осталось разобраться с отловом ошибок, упаковкой тарифов в json и кодировкой.

--- testTask/app/Http/Controllers/ApiController.php
+++ testTask/app/Http/Controllers/ApiController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tarifs;
+use App\Models\Services;
 
 class ApiController extends Controller
 {
-    public function GetServicesTarifs($user_id, $serivice_id) {
+    public function GetServicesTarifs($user_id, $service_id) {
+        $results = Tarifs::GetTarifsByGroup($service_id);
+        $str = "";
+        //return view("temp")->with(["collection"=>$results, "someText"=>Services::GetServiceById($service_id)]);
 
+        $tarifs = Tarifs::GetTarifsByGroup($service_id);
+        return response()->json([
+            "title" => $tarifs[0]->title,
+            "link" => $tarifs[0]->link,
+            "speed" => $tarifs[0]->speed,
+            "tarifs" => ["1", "2"]
+        ], 200);
     }
 
-    public function PutServicesTarif($user_id, $serivice_id){
+    public function PutServicesTarif($user_id, $service_id){
 
     }
 }
--- testTask/app/Models/Tarifs.php
+++ testTask/app/Models/Tarifs.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tarifs extends Model
 {
     use HasFactory;
+    protected $table = "tarifs";
+
+    public static function GetTarifsByGroup($groupId){
+        return Tarifs::select()->where("tarif_group_id", $groupId)->get();
+    }
+
 }
